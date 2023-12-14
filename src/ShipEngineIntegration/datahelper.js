72985a66b967179eae668a08a8c29f72c3553217
jahir-biz <jahir.uddin@bizcloudexperts.com>
2023-12-14 15:51:01 +0530

    code fix

--- src/ShipEngineIntegration/datahelper.js
+++ src/ShipEngineIntegration/datahelper.js
@@ -1,6 +1,6 @@
-const AWS = require('aws-sdk');
-const xml2js = require('xml2js');
-const { get, map } = require('lodash');
+const AWS = require("aws-sdk");
+const xml2js = require("xml2js");
+const { get, map } = require("lodash");
 const sns = new AWS.SNS();
 
 
@@ -8,139 +8,145 @@ async function ShipEnginePayload(xmlData) {
     try {
         const parser = new xml2js.Parser({ explicitArray: false, mergeAttrs: true });
         let xmnlObj = await parser.parseStringPromise(xmlData);
-        xmnlObj = get(xmnlObj, "UniversalInterchange.Body")
+        xmnlObj = get(xmnlObj, "UniversalInterchange.Body");
+        console.info(`🙂 -> file: datahelper.js:664 -> xmnlObj:`, JSON.stringify(xmnlObj));
 
         if (!get(xmnlObj, "UniversalShipment")) {
-            throw new Error('Invalid XML format or missing UniversalShipment element.');
+            throw new Error("Invalid XML format or missing UniversalShipment element.");
         }
-        const addressResidentialIndicator = get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].IsResidential', '');
 
-        // Check if the value is falsy, then set to 'no'
-        const addressResidentialValue = addressResidentialIndicator ? 'no' : addressResidentialIndicator;
+        const consigneeAddress = get(xmnlObj, "UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress", []).filter((add) => get(add, "AddressType") === "ConsigneeAddress")[0];
+        console.info(`🙂 -> file: datahelper.js:673 -> consigneeAddress:`, consigneeAddress);
+        const addressResidentialIndicator = get(consigneeAddress, "IsResidential", false) === false || get(consigneeAddress, "IsResidential", "false") === "false" ? "no" : "yes";
+        console.info(`🙂 -> file: datahelper.js:675 -> addressResidentialIndicator:`, addressResidentialIndicator);
 
-        const transportCompany = get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[0].OrganizationCode', '');
-        const serviceLevel = get(xmnlObj, 'UniversalShipment.Shipment.CarrierServiceLevel.Code', '');
-
+        const transportCompanyDocumentaryAddress = get(xmnlObj, "UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress", []).filter((add) => get(add, "AddressType") === "TransportCompanyDocumentaryAddress")[0];
+        console.info(`🙂 -> file: datahelper.js:23 -> transportCompanyDocumentaryAddress:`, transportCompanyDocumentaryAddress);
+        const transportCompany = get(transportCompanyDocumentaryAddress, "OrganizationCode", "");
+        console.info(`🙂 -> file: datahelper.js:1017 -> transportCompany:`, transportCompany);
+        const serviceLevel = get(xmnlObj, "UniversalShipment.Shipment.CarrierServiceLevel.Code", "");
+        console.info(`🙂 -> file: datahelper.js:1019 -> serviceLevel:`, serviceLevel);
+        const serviceCode = getServiceCode(transportCompany, serviceLevel);
+        console.info(`🙂 -> file: datahelper.js:1021 -> serviceCode:`, serviceCode);
         const Payload = {
-            label_download_type: 'inline',
+            label_download_type: "inline",
             shipment: {
                 ship_from: {
-                    name: 'Omni Logistics',
-                    phone: '650-555-1212',
-                    address_line1: '970 Harding Highway, Suite 200',
-                    city_locality: 'Penns Grove',
-                    state_province: 'NJ',
-                    postal_code: '08069',
-                    country_code: 'US',
-                    address_residential_indicator: 'no',
+                    name: "Omni Logistics",
+                    phone: "650-555-1212",
+                    address_line1: "970 Harding Highway, Suite 200",
+                    city_locality: "Penns Grove",
+                    state_province: "NJ",
+                    postal_code: "08069",
+                    country_code: "US",
+                    address_residential_indicator: "no",
                 },
-                external_shipment_id: get(xmnlObj, 'UniversalShipment.Shipment.DataContext.DataSourceCollection.DataSource.Key', ''),
-                confirmation: getConfirmation(xmnlObj, 'UniversalShipment.Shipment.IsSignatureRequired'),
-                shipment_number: get(xmnlObj, 'UniversalShipment.Shipment.Order.OrderNumber', ''),
-                external_order_id: get(xmnlObj, 'UniversalShipment.Shipment.Order.ClientReference', ''),
-                items: map(get(xmnlObj, 'UniversalShipment.Shipment.Order.OrderLineCollection.OrderLine', []), orderLine => ({
-                    sku: get(orderLine, 'Product.Code', ''),
-                    name: get(orderLine, 'Product.Description', ''),
-                    quantity: parseFloat(get(orderLine, 'QuantityMet', 0)),
+                external_shipment_id: get(xmnlObj, "UniversalShipment.Shipment.DataContext.DataSourceCollection.DataSource.Key", ""),
+                confirmation: getConfirmation(xmnlObj, "UniversalShipment.Shipment.IsSignatureRequired"),
+                shipment_number: get(xmnlObj, "UniversalShipment.Shipment.Order.OrderNumber", ""),
+                external_order_id: get(xmnlObj, "UniversalShipment.Shipment.Order.ClientReference", ""),
+                items: map(get(xmnlObj, "UniversalShipment.Shipment.Order.OrderLineCollection.OrderLine", []), (orderLine) => ({
+                    sku: get(orderLine, "Product.Code", ""),
+                    name: get(orderLine, "Product.Description", ""),
+                    quantity: parseFloat(get(orderLine, "QuantityMet", 0)),
                 })),
-                service_code: getServiceCode(transportCompany, serviceLevel),
+                service_code: serviceCode,
                 ship_to: {
-                    email: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Email', ''),
-                    address_line3: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].AdditionalAddressInformation', ''),
-                    address_line1: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Address1', ''),
-                    address_line2: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Address2', ''),
-                    city_locality: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].City', ''),
-                    company_name: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].CompanyName', ''),
-                    name: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Contact', ''),
-                    country_code: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Country.Code', ''),
-                    address_residential_indicator: addressResidentialValue,
-                    phone: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Phone', ''),
-                    postal_code: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].Postcode', ''),
-                    state_province: get(xmnlObj, 'UniversalShipment.Shipment.OrganizationAddressCollection.OrganizationAddress[1].State._', ''),
+                    email: get(consigneeAddress, "Email", ""),
+                    address_line3: get(consigneeAddress, "AdditionalAddressInformation", ""),
+                    address_line1: get(consigneeAddress, "Address1", ""),
+                    address_line2: get(consigneeAddress, "Address2", ""),
+                    city_locality: get(consigneeAddress, "City", ""),
+                    company_name: get(consigneeAddress, "CompanyName", ""),
+                    name: get(consigneeAddress, "Contact", ""),
+                    country_code: get(consigneeAddress, "Country.Code", ""),
+                    address_residential_indicator: addressResidentialIndicator,
+                    phone: get(consigneeAddress, "Phone", ""),
+                    postal_code: get(consigneeAddress, "Postcode", ""),
+                    state_province: get(consigneeAddress, "State._", ""),
                 },
-                packages: map(get(xmnlObj, 'UniversalShipment.Shipment.PackingLineCollection.PackingLine', []), packingLine => ({
+                packages: map(get(xmnlObj, "UniversalShipment.Shipment.PackingLineCollection.PackingLine", []), (packingLine) => ({
                     weight: {
-                        value: parseFloat(get(packingLine, 'Weight', 0)),
-                        unit: 'pound',
+                        value: parseFloat(get(packingLine, "Weight", 0)),
+                        unit: "pound",
                     },
                     dimensions: {
-                        height: parseFloat(get(packingLine, 'Height', 0)),
-                        width: parseFloat(get(packingLine, 'Width', 0)),
-                        length: parseFloat(get(packingLine, 'Length', 0)),
-                        unit: 'inch',
+                        height: parseFloat(get(packingLine, "Height", 0)),
+                        width: parseFloat(get(packingLine, "Width", 0)),
+                        length: parseFloat(get(packingLine, "Length", 0)),
+                        unit: "inch",
                     },
                     label_messages: {
-                        reference1: `${get(xmnlObj, 'UniversalShipment.Shipment.Order.OrderNumber', '')},${get(xmnlObj, 'UniversalShipment.Shipment.DataContext.DataSourceCollection.DataSource.Key', '')},${get(xmnlObj, 'UniversalShipment.Shipment.Order.ClientReference', '')}`,
+                        reference1: `${get(xmnlObj, "UniversalShipment.Shipment.Order.OrderNumber", "")},${get(xmnlObj, "UniversalShipment.Shipment.DataContext.DataSourceCollection.DataSource.Key", "")},${get(xmnlObj, "UniversalShipment.Shipment.Order.ClientReference", "")}`,
                     },
                 })),
             },
         };
         console.log(JSON.stringify(Payload));
-        return Payload;
+        return { shipenginePayload: Payload, skip: !serviceCode };
     } catch (error) {
-        console.error('Error in ShipEnginePayload:', error.message);
+        console.error("Error in ShipEnginePayload:", error.message);
         throw error;
     }
 }
 
 const getConfirmation = (obj, path) => {
     const isSignatureRequired = get(obj, path);
-    return isSignatureRequired === 'true' ? 'signature' : 'delivery';
+    return isSignatureRequired === "true" ? "signature" : "delivery";
 };
 
 const getServiceCode = (transportCompany, serviceLevel) => {
     const serviceCodeMappings = {
         UPSAIR: {
-            U1D: 'ups_next_day_air_saver',
-            U2D: 'ups_2nd_day_air',
-            U3D: 'ups_3_day_select',
-            UPS: 'ups_ground',
-            GRD: 'ups_ground',
-            STD: 'ups_ground',
+            U1D: "ups_next_day_air_saver",
+            U2D: "ups_2nd_day_air",
+            U3D: "ups_3_day_select",
+            UPS: "ups_ground",
+            GRD: "ups_ground",
+            STD: "ups_ground",
         },
         DHLWORIAH: {
-            STD: 'UNKNOWN',
+            STD: "UNKNOWN",
         },
         FEDEXMEM: {
-            STD: 'fedex_ground',
+            STD: "fedex_ground",
         },
     };
 
-    return serviceCodeMappings?.[transportCompany]?.[serviceLevel] || '';
+    return serviceCodeMappings[transportCompany]?.[serviceLevel] ?? false;
 };
-
 
 function labelEventPayload(data, shipment_id) {
     try {
         const builder = new xml2js.Builder({
             headless: true,
-            renderOpts: { pretty: true, indent: '    ' },
+            renderOpts: { pretty: true, indent: "    " },
         });
         const xmlData = {
             UniversalEvent: {
-                $: { xmlns: 'http://www.cargowise.com/Schemas/Universal/2011/11', version: '1.1' },
+                $: { xmlns: "http://www.cargowise.com/Schemas/Universal/2011/11", version: "1.1" },
                 Event: {
                     DataContext: {
                         DataTargetCollection: {
                             DataTarget: {
-                                Type: 'WarehouseOrder',
+                                Type: "WarehouseOrder",
                                 Key: shipment_id,
                             },
                         },
                     },
                     EventTime: data.created_at,
-                    EventType: 'DDI',
-                    EventReference: 'LBL',
+                    EventType: "DDI",
+                    EventReference: "LBL",
                     IsEstimate: false,
                     AttachedDocumentCollection: {
                         AttachedDocument: {
                             FileName: `label ${shipment_id}.pdf`,
                             ImageData: {
                                 $: {},
-                                _: data.label_download.href.split(',')[1], // extracting base64 data
+                                _: data.label_download.href.split(",")[1], // extracting base64 data
                             },
                             Type: {
-                                Code: 'LBL',
+                                Code: "LBL",
                             },
                             IsPublished: true,
                         },
@@ -151,7 +157,7 @@ function labelEventPayload(data, shipment_id) {
 
         return builder.buildObject(xmlData);
     } catch (error) {
-        console.error('Error in labelEventPayload:', error);
+        console.error("Error in labelEventPayload:", error);
         throw error;
     }
 }
@@ -160,18 +166,18 @@ function trackingShipmentPayload(data, shipment_id, OrderNumber) {
     try {
         const builder = new xml2js.Builder({
             headless: true,
-            renderOpts: { pretty: true, indent: '    ' },
+            renderOpts: { pretty: true, indent: "    " },
         });
 
         const xmlData = {
             UniversalShipment: {
-                $: { 'xmlns:ns0': 'http://www.cargowise.com/Schemas/Universal/2011/11' },
+                $: { "xmlns:ns0": "http://www.cargowise.com/Schemas/Universal/2011/11" },
                 Shipment: {
-                    $: { xmlns: 'http://www.cargowise.com/Schemas/Universal/2011/11' },
+                    $: { xmlns: "http://www.cargowise.com/Schemas/Universal/2011/11" },
                     DataContext: {
                         DataTargetCollection: {
                             DataTarget: {
-                                Type: 'WarehouseOrder',
+                                Type: "WarehouseOrder",
                                 Key: shipment_id,
                             },
                         },
@@ -185,7 +191,7 @@ function trackingShipmentPayload(data, shipment_id, OrderNumber) {
         };
         return builder.buildObject(xmlData);
     } catch (error) {
-        console.error('Error in trackingShipmentPayload:', error);
+        console.error("Error in trackingShipmentPayload:", error);
         throw error;
     }
 }
@@ -199,10 +205,10 @@ async function sendSNSNotification(subject, message) {
 
     try {
         await sns.publish(params).promise();
-        console.log('SNS notification sent successfully.');
+        console.log("SNS notification sent successfully.");
     } catch (snsError) {
-        console.error('Error sending SNS notification:', snsError);
+        console.error("Error sending SNS notification:", snsError);
     }
 }
 
-module.exports = { ShipEnginePayload, labelEventPayload, trackingShipmentPayload, sendSNSNotification }
+module.exports = { ShipEnginePayload, labelEventPayload, trackingShipmentPayload, sendSNSNotification };
--- src/ShipEngineIntegration/index.js
+++ src/ShipEngineIntegration/index.js
@@ -1,54 +1,57 @@
-const AWS = require('aws-sdk');
-const { get } = require('lodash');
-const uuid = require('uuid');
-const { ShipEnginePayload, labelEventPayload, trackingShipmentPayload, sendSNSNotification } = require("./datahelper")
-const { updateApiStatus, insertApiStatus } = require("./dynamo")
-const { getS3Object, makeAndStoreApiCall } = require("./requestor")
+const AWS = require("aws-sdk");
+const { get } = require("lodash");
+const uuid = require("uuid");
+const { ShipEnginePayload, labelEventPayload, trackingShipmentPayload, sendSNSNotification } = require("./datahelper");
+const { updateApiStatus, insertApiStatus } = require("./dynamo");
+const { getS3Object, makeAndStoreApiCall } = require("./requestor");
 
 module.exports.handler = async (event, context) => {
-  console.info(`🙂 -> file: index.js:9 -> event:`, JSON.stringify(event));
-  let apiStatusId;
-  let externalShipmentId;
-  let shipmentNumber;
-  try {
-    // Extract XML file from S3 event
-    const s3Bucket = get(event, 'Records[0].s3.bucket.name', '');
-    const s3Key = get(event, 'Records[0].s3.object.key', '');
-    apiStatusId = uuid.v4();
-    // Get XML data from S3
-    const xmlData = await getS3Object(s3Bucket, s3Key);
-    // Parse XML and construct Shipengine API payload
-    const shipenginePayload = await ShipEnginePayload(xmlData);
-    // Extract externalShipmentId, shipmentNumber from the payload for correlation
-    externalShipmentId = get(shipenginePayload, 'shipment.external_shipment_id', '');
-    shipmentNumber = get(shipenginePayload, 'shipment.shipment_number', '');
-    // Insert into ApiStatusTable with status "Processing"
-    await insertApiStatus(apiStatusId, 'Processing', externalShipmentId);
-    // Make a request to Shipengine API
-    const shipengineResponse = await makeAndStoreApiCall('ShipEngine', shipenginePayload, apiStatusId, externalShipmentId);
-    // Parse Shipengine API response and construct payloads for Eadapter APIs
-    const addLabelPayload = labelEventPayload(shipengineResponse, externalShipmentId);
-    const trackingPayload = trackingShipmentPayload(shipengineResponse, externalShipmentId, shipmentNumber);
-    // Using Promise.all for parallel execution of Cargowise API calls.
-    const CargowiseApiCalls = [
-      makeAndStoreApiCall('AddDocument', addLabelPayload, apiStatusId, externalShipmentId),
-      makeAndStoreApiCall('AddTracking', trackingPayload, apiStatusId, externalShipmentId),
-    ];
+    console.info(`🙂 -> file: index.js:9 -> event:`, JSON.stringify(event));
+    let apiStatusId;
+    let externalShipmentId;
+    let shipmentNumber;
     try {
-      await Promise.all(CargowiseApiCalls);
+        // Extract XML file from S3 event
+        const s3Bucket = get(event, "Records[0].s3.bucket.name", "");
+        const s3Key = get(event, "Records[0].s3.object.key", "");
+        apiStatusId = uuid.v4();
+        // Get XML data from S3
+        const xmlData = await getS3Object(s3Bucket, s3Key);
+        // Parse XML and construct Shipengine API payload
+        const { shipenginePayload, skip } = await ShipEnginePayload(xmlData);
+        // Extract externalShipmentId, shipmentNumber from the payload for correlation
+        externalShipmentId = get(shipenginePayload, "shipment.external_shipment_id", "");
+        shipmentNumber = get(shipenginePayload, "shipment.shipment_number", "");
+        // Insert into ApiStatusTable with status "Processing"
+        await insertApiStatus(apiStatusId, "PROCESSING", externalShipmentId);
+
+        if (skip) {
+          await updateApiStatus(apiStatusId, "StatusUpdate", "SKIPPED", externalShipmentId);
+          await updateApiStatus(apiStatusId, "ErrorMessage", "Valid service level not present.", externalShipmentId);
+          return "SKIPPED: Valid service level not present.";
+        }
+        // Make a request to Shipengine API
+        const shipengineResponse = await makeAndStoreApiCall("ShipEngine", shipenginePayload, apiStatusId, externalShipmentId);
+        // Parse Shipengine API response and construct payloads for Eadapter APIs
+        const addLabelPayload = labelEventPayload(shipengineResponse, externalShipmentId);
+        const trackingPayload = trackingShipmentPayload(shipengineResponse, externalShipmentId, shipmentNumber);
+        // Using Promise.all for parallel execution of Cargowise API calls.
+        const CargowiseApiCalls = [makeAndStoreApiCall("AddDocument", addLabelPayload, apiStatusId, externalShipmentId), makeAndStoreApiCall("AddTracking", trackingPayload, apiStatusId, externalShipmentId)];
+        try {
+            await Promise.all(CargowiseApiCalls);
+        } catch (error) {
+            console.error("Error in CargowiseApiCalls:", error);
+        }
+        // Update the status to 'Processed' in ApiStatusTable
+        await updateApiStatus(apiStatusId, "StatusUpdate", "PROCESSED", externalShipmentId);
+        console.info("All the API calls are successful");
     } catch (error) {
-      console.error('Error in CargowiseApiCalls:', error);
+        console.error("Error:", error);
+        // Update the status to 'failure' and log error message
+        await updateApiStatus(apiStatusId, "StatusUpdate", "FAILED", externalShipmentId);
+        await updateApiStatus(apiStatusId, "ErrorMessage", error.message, externalShipmentId);
+        // Send SNS notification
+        await sendSNSNotification(`Error occurred in Lambda function ${context.functionName}`, error.message);
+        throw error;
     }
-    // Update the status to 'Processed' in ApiStatusTable
-    await updateApiStatus(apiStatusId, 'StatusUpdate', 'Processed', externalShipmentId);
-    console.info('All the API calls are successful');
-  } catch (error) {
-    console.error('Error:', error);
-    // Update the status to 'failure' and log error message
-    await updateApiStatus(apiStatusId, 'StatusUpdate', 'failure', externalShipmentId);
-    await updateApiStatus(apiStatusId, 'ErrorMessage', error.message, externalShipmentId);
-    // Send SNS notification
-    await sendSNSNotification(`Error occurred in Lambda function ${context.functionName}`, error.message);
-    throw error;
-  }
 };
